5fe26740b2de91ebb6f9dbaaf14f32b2fbde9bee
Felipe Calvache <felipe.calvache-g@mail.escuelaing.edu.co>
2025-08-21 14:41:42 -0500

    Subiendo trabajo

---
+++ Laboratorio-2-CVDS-DOSW-01/Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Laboratorio02DoswApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio02DoswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio02DoswApplication.class, args);
+	}
+
+}
---
+++ Laboratorio-2-CVDS-DOSW-01/Laboratorio-2-CVDS-DOSW-01/src/test/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Laboratorio02DoswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_2_CVDS_DOSW_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio02DoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
